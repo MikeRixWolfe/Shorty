35ee1826aae4c638da9841586de452b4e75f6b0a
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2022-07-03 01:16:37 -0500

    a bit of fun

--- app/views/index.py
+++ app/views/index.py
@@ -1,8 +1,12 @@
 from flask import render_template
+from random import choice
 from app import app
 
 
 @app.route('/', strict_slashes=False, methods=['GET'])
 def index():
-    return render_template('index.html')
+    greetings = ['You seem to have taken a wrong turn...',
+        'Orcs attack, roll initiative',
+        'Speak friend and enter']
+    return render_template('index.html', text=choice(greetings))
 
